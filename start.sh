72c15869c75eb5fb11349e3edb97f464f3d40914
Eugene Stepanov <evgeniy@goldenplanet.com>
2015-11-22 11:59:41 +0200

    added env variables

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export REDIS_HOST=REDIS_PORT_6379_TCP_ADDR
-export REDIS_PORT=REDIS_PORT_6379_TCP_PORT
+export REDIS_HOST=${REDIS_PORT_6379_TCP_ADDR}
+export REDIS_PORT=${REDIS_PORT_6379_TCP_PORT}
 
 /usr/bin/nodejs token-scraper-redis.js 8666
